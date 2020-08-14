712577c2671d75a88906e3abfe74b1f83f337c21
gatheluck <gatheluck+tech@gmail.com>
2020-08-14 16:50:25 +0900

    add fourier_heatmap_fba_dataset.py

--- misc/data.py
+++ misc/data.py
@@ -1,47 +1,71 @@
 import os
 import sys
 
-base = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+base = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(base)
 
-from collections import namedtuple
-
+import hydra
+import omegaconf
 import torch
 import torchvision
 
+
 class DatasetBuilder(object):
-    # tuple for dataset config
-    DC = namedtuple('DatasetConfig', ['mean', 'std', 'input_size', 'num_classes'])
-    
-    DATASET_CONFIG = {
-        'svhn' :       DC([0.43768210, 0.44376970, 0.47280442], [0.19803012, 0.20101562, 0.19703614], 32, 10),
-        'cifar10':     DC([0.49139968, 0.48215841, 0.44653091], [0.24703223, 0.24348513, 0.26158784], 32, 10),
-        'imagenet100': DC([0.485, 0.456, 0.406], [0.229, 0.224, 0.225], 224, 100),
-        'imagenet':    DC([0.485, 0.456, 0.406], [0.229, 0.224, 0.225], 224, 1000),
-    } 
+    # fbdb (FourierBasisDB) is original formula driven dataset generated from Fourier Basis.
+    # about fbdb, please check https://github.com/gatheluck/FourierBasisDB.
+    SUPPORTED_DATASET = set('svhn cifar10 imagenet100 imagenet fbdb'.split())
 
-    def __init__(self, name:str, root_path:str):
+    def __init__(self, **kwargs):
         """
         Args
-        - name: name of dataset
-        - root_path: root path to datasets
+        - name (str)       : name of dataset
+        - input_size (int) : input image size
+        - mean (tuple)     : mean of normalized pixel value of channels
+        - std (tuple)      : standard deviation of normalized pixel value of channels
+        - root_path (str)  : root path to dataset
         """
-        if name not in self.DATASET_CONFIG.keys():
-            raise ValueError('name of dataset is invalid')
-        self.name = name
-        self.root_path = os.path.join(root_path, self.name)
+        required_keys = set('name input_size mean std root_path'.split())
+        parsed_args = self._parse_args(required_keys, kwargs)
 
-    def __call__(self, train:bool, normalize:bool, binary_classification_target:int=None, optional_transform=[]):
+        for k, v in parsed_args.items():
+            print('{}:{}'.format(k, v))
+            setattr(self, k, v)  # set required_keys as attribute
+
+        self.root_path = os.path.join(self.root_path, self.name)
+
+        assert (self.name in self.SUPPORTED_DATASET) or ('fbdb' in self.name), 'name of dataset is invalid.'
+        assert self.input_size > 0, 'input_size should be larger than 0.'
+        assert len(self.mean) == len(self.std), 'length of mean and std should be same.'
+
+    def _parse_args(self, required_keys: set, input_args: dict) -> dict:
+        """
+        parse input args
+        Args
+        - required_keys (set) : set of required keys for input_args
+        - input_args (dict)   : dict of input arugments
+        """
+        parsed_args = dict()
+
+        for k in required_keys:
+            if k not in input_args.keys():
+                raise ValueError('initial args are invalid.')
+            else:
+                parsed_args[k] = input_args[k]
+
+        return parsed_args
+
+    def __call__(self, train: bool, normalize: bool, num_samples: int = -1, binary_target: int = None, corruption_type: str = None, optional_transform=[], **kwargs):
         """
         Args
-        - train : use train set or not.
-        - normalize : do normalize or not.
-        - binary_classification_target : if not None, creates datset for binary classification.
+        - train (bool)              : use train set or not.
+        - normalize (bool)          : do normalize or not.
+        - num_samples (int)         : number of samples. if -1, it means use all samples. 
+        - binary_target (int)       : if not None, creates datset for binary classification.
+        - corruption_type (str)     : type of corruption. only avilable for cifar10c.
+        - optional_transform (list) : list of optional transformations. these are applied before normalization.
         """
-        
-        input_size = self.DATASET_CONFIG[self.name].input_size
-        transform = self._get_transform(self.name, input_size, train, normalize, optional_transform)
-        
+        transform = self._get_transform(self.name, self.input_size, self.mean, self.std, train, normalize, optional_transform)
+
         # get dataset
         if self.name == 'svhn':
             dataset = torchvision.datasets.SVHN(root=self.root_path, split='train' if train else 'test', transform=transform, download=True)
@@ -49,44 +73,67 @@ class DatasetBuilder(object):
         elif self.name == 'cifar10':
             dataset = torchvision.datasets.CIFAR10(root=self.root_path, train=train, transform=transform, download=True)
             targets_name = 'targets'
-        elif self.name in 'imagenet100 imagenet'.split():
+        elif (self.name in 'imagenet100 imagenet'.split()) or ('fbdb' in self.name):
             root = os.path.join(self.root_path, 'train' if train else 'val')
             dataset = torchvision.datasets.ImageFolder(root, transform=transform)
-        else: 
-            raise NotImplementedError 
+        else:
+            raise NotImplementedError
 
         # make binary classification dataset
-        if binary_classification_target is not None:
-            targets = getattr(dataset, targets_name)
-            assert binary_classification_target <= max(targets)
+        if binary_target is not None:
+            dataset = self._binarize_dataset(dataset, targets_name, binary_target)
 
-            targets = [1 if target==binary_classification_target else 0 for target in targets]
-            setattr(dataset, targets_name, targets)
+        # take subset
+        if num_samples != -1:
+            assert num_samples > 0, 'num_samples should be larger than 0 or -1'
+            num_samples = min(num_samples, len(dataset))
+            indices = [i for i in range(num_samples)]
+            dataset = torch.utils.data.Subset(dataset, indices)
 
         return dataset
 
-    def _get_transform(self, name:str, input_size:int, train:bool, normalize:bool, optional_transform=[]):
+    def _binarize_dataset(self, dataset, targets_name: str, binary_target: int):
         """
-        input_size
-        - cifar10, svhn: 32x32
-        - imagenet100, imagenet: 224x224  
+        Args
+        - dataset             : pytorch dataset class.
+        - targets_name (str)  : intermediate variable to compensate inconsistent torchvision API.
+        - binary_target (int) : true class label of binary classification.
+        """
+        targets = getattr(dataset, targets_name)
+        assert 0 <= binary_target <= max(targets)
+
+        targets = [1 if target == binary_target else 0 for target in targets]
+        setattr(dataset, targets_name, targets)
+
+        return dataset
+
+    def _get_transform(self, name: str, input_size: int, mean: tuple, std: tuple, train: bool, normalize: bool, optional_transform=[]):
+        """
+        Args
+        - name (str)                : name of dataset.
+        - input_size (int)          : input image size.
+        - mean (tuple)              : mean of normalized pixel value of channels
+        - std (tuple)               : standard deviation of normalized pixel value of channels
+        - train (bool)              : use train set or not.
+        - normalize (bool)          : normalize image or not.
+        - optional_transform (list) : list of optional transformations. these are applied before normalization.
         """
         transform = []
 
         # arugmentation
-        # imagenet100 / imagenet
+        # imagenet100 / imagenet / fbdb
         if input_size == 224:
             if train:
                 transform.extend([
                     torchvision.transforms.RandomResizedCrop(224),
-				    torchvision.transforms.RandomHorizontalFlip(),
+                    torchvision.transforms.RandomHorizontalFlip(),
                 ])
             else:
                 transform.extend([
                     torchvision.transforms.Resize(256),
                     torchvision.transforms.CenterCrop(224),
                 ])
-        # cifar10 / svhn
+        # cifar10 / cifar10c  / svhn / fbdb
         elif input_size == 32:
             if train:
                 transform.extend([
@@ -99,7 +146,7 @@ class DatasetBuilder(object):
             raise NotImplementedError
 
         # to tensor
-        transform.extend([torchvision.transforms.ToTensor(),])
+        transform.extend([torchvision.transforms.ToTensor(), ])
 
         # optional (Fourier Noise, Patch Shuffle, etc.)
         if optional_transform:
@@ -108,25 +155,25 @@ class DatasetBuilder(object):
         # normalize
         if normalize:
             transform.extend([
-                torchvision.transforms.Normalize(mean=self.DATASET_CONFIG[name].mean, std=self.DATASET_CONFIG[name].std),
+                torchvision.transforms.Normalize(mean=mean, std=std),
             ])
 
         return torchvision.transforms.Compose(transform)
-    
-    @property
-    def input_size(self):
-        return self.DATASET_CONFIG[self.name].input_size
 
-    @property
-    def num_classes(self):
-        return self.DATASET_CONFIG[self.name].num_classes
+
+@hydra.main(config_path='./conf/config.yaml')
+def test(cfg: omegaconf.DictConfig):
+    print(cfg.pretty())
+
+    dataset_builder = DatasetBuilder(root_path=os.path.join(hydra.utils.get_original_cwd(), './data'), **cfg.dataset)
+    print(dataset_builder.name)
+    print(dataset_builder.input_size)
+    print(dataset_builder.mean)
+    print(dataset_builder.std)
+
+    test_set = dataset_builder(train=False, normalize=True, **cfg.dataset)
+    print(len(test_set))
+
 
 if __name__ == '__main__':
-
-    dataset_builder = DatasetBuilder(name='cifar10', root_path='../data')
-    test_set = dataset_builder(train=False, normalize=True)
-    print(test_set.targets)
-
-    dataset_builder = DatasetBuilder(name='cifar10', root_path='../data')
-    test_set = dataset_builder(train=False, normalize=True, binary_classification_target=7)
-    print(test_set.targets)
+    test()
