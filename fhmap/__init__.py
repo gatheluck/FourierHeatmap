d5efeeaa49c0a53f98a279062587476935885eb1
gatheluck <gatheluck+tech@gmail.com>
2021-04-18 20:46:25 +0900

    refactor: move eval_mean_errors from eval_fhmap.py.

--- fhmap/__init__.py
+++ fhmap/__init__.py
@@ -1,7 +1,9 @@
 import logging
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
 import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -10,10 +12,10 @@ logging.basicConfig(level=logging.INFO)
 def calc_errors(
     output: torch.Tensor, target: torch.Tensor, topk: Tuple[int, ...] = (1,)
 ) -> List[torch.Tensor]:
-    """Calculate top-k errors.
+    """Calculate top-k errors over output from architecture (model).
 
     Args:
-        output (torch.Tensor): Output tensor from model.
+        output (torch.Tensor): Output tensor from architecture (model).
         target (torch.Tensor): Training target tensor.
         topk (Tuple[int, ...]): Tuple of int which you want to know error.
 
@@ -38,3 +40,34 @@ def calc_errors(
             errors.append(wrong_k.mul_(100.0 / batch_size))
 
         return errors
+
+
+def eval_mean_errors(
+    arch: nn.Module,
+    loader: DataLoader,
+    device: torch.device,
+    topk: Tuple[int, ...] = (1,)
+) -> List[float]:
+    """Evaluate top-k mean errors of the architecture over given dataloader.
+
+    Args:
+        arch (nn.Module): An architecture to be evaluated.
+        loader (DataLoader): A dataloader.
+        device (torch.device): A device used for calculation.
+        topk (Tuple[int, ...]): Tuple of int which you want to know error.
+
+    Returns:
+        List[float]: list of mean errors.
+
+    """
+    arch = arch.to(device)
+    err_dict: Dict[int, List[float]] = {k: list() for k in topk}
+
+    for x, t in loader:
+        x, t = x.to(device), t.to(device)
+
+        output = arch(x)
+        for k, err in zip(topk, calc_errors(output, t, topk=topk)):
+            err_dict[k].append(err.item())
+
+    return [sum(err_dict[k]) / len(err_dict[k]) for k in err_dict]
