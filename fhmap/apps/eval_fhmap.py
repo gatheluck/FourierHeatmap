de222b469d5fd2377f6c3e9bb0181fe2ffe982db
gatheluck <gatheluck+tech@gmail.com>
2021-04-18 20:49:10 +0900

    refactor: move main processes to fourier/heatmap.

--- fhmap/apps/eval_fhmap.py
+++ fhmap/apps/eval_fhmap.py
@@ -1,91 +1,19 @@
 import logging
 import pathlib
-from collections import OrderedDict
 from dataclasses import dataclass
 from typing import Final, Tuple, cast
 
 import hydra
-import matplotlib.pyplot as plt
-import seaborn as sns
 import torch
-import torch.nn as nn
 from hydra.core.config_store import ConfigStore
 from hydra.utils import instantiate
 from omegaconf import MISSING, OmegaConf
-from torch.utils.data import DataLoader
-from tqdm import tqdm
 
-import fhmap
-import fhmap.fourier as fourier
+import fhmap.fourier.heatmap as heatmap
 import fhmap.schema as schema
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-
-
-def eval_error(
-    arch: nn.Module,
-    loader: DataLoader,
-    device: torch.device,
-) -> Tuple[float, float]:
-    """Return top1 and top5 mean errors.
-
-    Args:
-        arch (nn.Module): The architecture to be evaluated.
-        loader (DataLoader): The data loader
-        device (torch.device): The device used for calculation.
-
-    """
-    arch = arch.to(device)
-    err1_list, err5_list = list(), list()
-
-    for x, t in loader:
-        x, t = x.to(device), t.to(device)
-
-        output = arch(x)
-        err1, err5 = fhmap.calc_errors(output, t, topk=(1, 5))
-        err1_list.append(err1.item())
-        err5_list.append(err5.item())
-
-    mean_err1 = sum(err1_list) / len(err1_list)
-    mean_err5 = sum(err5_list) / len(err5_list)
-    return mean_err1, mean_err5
-
-
-def save_fhmap(
-    error_matrix: torch.Tensor, savedir: pathlib.Path, suffix: str = ""
-) -> None:
-    """Save Fourier Heat Map as a png image.
-
-    Args:
-        error_matrix (torch.Tensor): The matrix of errors. The size of error matrix should be (H, H/2+1). Here, H is height of image.
-        savedir (pathlib.Path): Path to the directory where the results will be saved.
-        suffix (str): Suffix which is attached to result file of Fourier Heat Map.
-
-    """
-    assert len(error_matrix.size()) == 2
-    assert error_matrix.size(0) == 2 * (error_matrix.size(1) - 1)
-
-    # drop the edge for fliping
-    error_matrix = error_matrix[1:, :-1]
-
-    # fill left side of error_matrix
-    error_matrix = torch.cat(
-        [torch.flip(error_matrix, (0, 1))[:, :-1], error_matrix], dim=1
-    )
-
-    torch.save(error_matrix, savedir / ("fhmap_data" + suffix + ".pth"))
-    sns.heatmap(
-        error_matrix.numpy(),
-        vmin=0.0,
-        vmax=1.0,
-        cmap="jet",
-        cbar=True,
-        xticklabels=False,
-        yticklabels=False,
-    )
-    plt.savefig(savedir / ("fhmap" + suffix + ".png"))
-    plt.close("all")  # this is needed for continuous figure generation.
 
 
 @dataclass
@@ -99,6 +27,7 @@ class EvalFhmapConfig:
         batch_size (int): The size of batch.
         num_samples (int): The  number of samples used from dataset. If -1, use all samples.
         eps (float): The L2 norm size of Fourier basis.
+        topk (Tuple): Tuple of int which you want to know error.
         weightpath (str): The path to pytorch model weight.
 
     """
@@ -110,6 +39,7 @@ class EvalFhmapConfig:
     batch_size: int = 512
     num_samples: int = -1
     eps: float = 4.0
+    topk: Tuple = (1, 5)
     weightpath: str = MISSING
 
 
@@ -119,10 +49,12 @@ cs.store(name="eval_fhmap", node=EvalFhmapConfig)
 cs.store(group="arch", name="resnet50", node=schema.Resnet50Config)
 cs.store(group="arch", name="resnet56", node=schema.Resnet56Config)
 cs.store(group="arch", name="wideresnet40", node=schema.Wideresnet40Config)
+# NOTE: If you want to add your custom architecture, please add YourCustomArchConfig as a node here.
 # dataset
 cs.store(group="dataset", name="cifar10", node=schema.Cifar10Config)
 cs.store(group="dataset", name="imagenet100", node=schema.Imagenet100Config)
 cs.store(group="dataset", name="imagenet", node=schema.ImagenetConfig)
+# NOTE: If you want to add your custom dataset, please add YourCustomDatasetConfig as a node here.
 # env
 cs.store(group="env", name="default", node=schema.DefaultEnvConfig)
 
@@ -150,7 +82,6 @@ def eval_fhmap(cfg: EvalFhmapConfig) -> None:
     device: Final = "cuda" if cfg.env.gpus > 0 else "cpu"
     cwd: Final[pathlib.Path] = pathlib.Path(hydra.utils.get_original_cwd())
     weightpath: Final[pathlib.Path] = pathlib.Path(cfg.weightpath)
-    savedir: Final[pathlib.Path] = pathlib.Path(cfg.env.savedir)
 
     # Setup datamodule
     root: Final[pathlib.Path] = cwd / "data"
@@ -163,43 +94,18 @@ def eval_fhmap(cfg: EvalFhmapConfig) -> None:
     arch = arch.to(device)
     arch.eval()
 
-    # height and width of error_matrix
-    # Note: Since Fourier Heat Map has origin symmetry, it is sufficient to calculate only half the region about width.
-    height: Final[int] = datamodule.input_size
-    width: Final[int] = height // 2 + 1
-    assert height % 2 == 0, "currently we only support even input size."
+    fhmaps = heatmap.eval_fourier_heatmap(
+        datamodule.input_size,
+        arch,
+        datamodule,
+        cfg.num_samples,
+        cfg.eps,
+        cast(torch.device, device),  # needed for passing mypy check.
+        cfg.topk,
+    )
 
-    error_matrix_top1 = torch.zeros(height * width).float()
-    error_matrix_top5 = torch.zeros(height * width).float()
-
-    spectrum = fourier.get_basis_spectrum(height, width, low_center=True)
-
-    with tqdm(
-        torch.chunk(
-            fourier.spectrum_to_basis(spectrum, l2_normalize=True),
-            spectrum.size(0),
-        ),
-        ncols=80,
-    ) as pbar:
-        for i, basis in enumerate(pbar):  # Size of basis is [1, height, width]
-            basis = basis.squeeze(0) * cfg.eps
-            datamodule.setup("test", basis=basis)
-            loader = datamodule.test_dataloader(cfg.num_samples)
-
-            mean_err1, mean_err5 = eval_error(arch, loader, cast(torch.device, device))
-            error_matrix_top1[i], error_matrix_top5[i] = mean_err1, mean_err5
-
-            results = OrderedDict()
-            results["err1"] = mean_err1
-            results["err5"] = mean_err5
-            pbar.set_postfix(results)
-            pbar.update()
-
-    error_matrix_top1 = error_matrix_top1.view(height, width)
-    error_matrix_top5 = error_matrix_top5.view(height, width)
-
-    save_fhmap(error_matrix_top1 / 100.0, savedir, "_top1")
-    save_fhmap(error_matrix_top5 / 100.0, savedir, "_top5")
+    for k, fourier_heatmap in zip(cfg.topk, fhmaps):
+        heatmap.save_fourier_heatmap(fourier_heatmap / 100.0, pathlib.Path("."), f"_top{k}")
 
 
 if __name__ == "__main__":
