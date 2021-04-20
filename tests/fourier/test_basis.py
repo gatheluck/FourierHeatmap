292a53614523eb39f11c60ced578bf5dea3d4bfc
gatheluck <gatheluck+tech@gmail.com>
2021-04-21 01:35:25 +0900

    test: update test code.

--- tests/fourier/test_basis.py
+++ tests/fourier/test_basis.py
@@ -10,35 +10,26 @@ from fhmap.fourier import basis
 class TestSpectrumToBasis:
     @pytest.fixture
     def sample_spectrum(self):
-        B, H, W = 16, 32, 17
-        return torch.rand(B, H, W)
+        H, W = 32, 17
+        return torch.rand(H, W)
 
     def test__output_size(self, sample_spectrum):
-        B, H, W = sample_spectrum.size()
+        H, W = sample_spectrum.size()
         assert basis.spectrum_to_basis(sample_spectrum, False).size() == torch.Size(
-            [B, H, H]
+            [H, H]
         )
         assert basis.spectrum_to_basis(sample_spectrum, True).size() == torch.Size(
-            [B, H, H]
+            [H, H]
         )
 
     def test__norm(self, sample_spectrum):
-        B, _, _ = sample_spectrum.size()
-        ones = torch.ones(B, dtype=torch.float)
+        one = torch.ones(1, dtype=torch.float)
         assert not torch.allclose(
-            basis.spectrum_to_basis(sample_spectrum, False).norm(dim=(-2, -1)), ones
+            basis.spectrum_to_basis(sample_spectrum, False).norm(dim=(-2, -1)), one
         )
         assert torch.allclose(
-            basis.spectrum_to_basis(sample_spectrum, True).norm(dim=(-2, -1)), ones
+            basis.spectrum_to_basis(sample_spectrum, True).norm(dim=(-2, -1)), one
         )
-
-    def test__consistency(self, sample_spectrum):
-        """Check if FFT is applied to each spectrum independently."""
-        B, _, _ = sample_spectrum.size()
-        for b in range(B):
-            assert basis.spectrum_to_basis(sample_spectrum)[b][None, :, :].allclose(
-                basis.spectrum_to_basis(sample_spectrum[b][None, :, :])
-            )
 
 
 class TestBasisSpectrum:
@@ -49,25 +40,5 @@ class TestBasisSpectrum:
 
     def test__output_size(self, sample_sizes):
         H, W = sample_sizes
-        assert basis.get_basis_spectrum(H, W).size() == torch.Size([H * W, H, W])
-
-    def test__visualize_basis(self, logdir):
-        logdir.mkdir(exist_ok=True, parents=True)
-
-        # high center
-        savepath = pathlib.Path(logdir) / "test_visualized_basis_high_center.png"
-        if savepath.exists():
-            savepath.unlink()
-
-        H, W = 32, 17
-        fourier_basis = basis.spectrum_to_basis(basis.get_basis_spectrum(H, W, False))
-        torchvision.utils.save_image(fourier_basis[:, None, :, :], savepath, nrow=W)
-
-        # low center
-        savepath = pathlib.Path(logdir) / "test_visualized_basis_low_center.png"
-        if savepath.exists():
-            savepath.unlink()
-
-        H, W = 32, 17
-        fourier_basis = basis.spectrum_to_basis(basis.get_basis_spectrum(H, W, True))
-        torchvision.utils.save_image(fourier_basis[:, None, :, :], savepath, nrow=W)
+        for b in basis.get_spectrum(H, W):
+            assert b.size() == torch.Size([H, W])
